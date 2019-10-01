e21c9715f52005f1c599053f5a8591e154ac2ffd
Garry <troet@live.de>
2019-10-01 16:12:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var frage2 = document.getElementById('frage2');
 
-function func1() {
+function func1(){
     var antwort = document.getElementById("antwort1");
     //if (antwort == new String("689")){
-        frage2.style.color = 'black';
+    frage2.style.color = 'black';
     //}
 }
