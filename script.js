ff7c4f8a849275db1c214b6f9dc7281af1faeae3
Garry <troet@live.de>
2019-10-01 18:46:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function func1(){
     var a1 = document.getElementById("a1");
     if (a1.value == "689"){
         f2.style.color = 'black';
+    } else if (a1.value == "000"){
+        f2.style.color = 'black';
     } else {
         a1.value = "";
     }
