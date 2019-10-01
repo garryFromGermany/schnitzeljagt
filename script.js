02ca4129ef9db971ea3b385ba19bb67e6b2c9f89
Garry <troet@live.de>
2019-10-01 17:35:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 var f2 = document.getElementById('frage2');
 var f3 = document.getElementById('frage3');
 var a1;
-var lsg;
+var lsg = "689";
 
 function func1(){
     a1 = document.getElementById("antwort1").value;
-    lsg = "689";
     if (a1.equals(lsg)){
         f2.style.color = 'black';
     } else {
