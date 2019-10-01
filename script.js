8172c688b4e561260dfbed68f5130cb7429ab0bb
Garry <troet@live.de>
2019-10-01 17:05:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ function func1(){
     var a1 = document.getElementById("antwort1");
     var lsg = "689";
     if (a1.equals(lsg)){
-        frage2.style.color = 'black';
+        f2.style.color = 'black';
     }
 }
