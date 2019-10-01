96c132edc52079d417a673447c1d1e317eb9eda8
Garry <troet@live.de>
2019-10-01 17:03:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var f2 = document.getElementById('frage2');
-var a1;
+
 
 function func1(){
-    a1 = document.getElementById("antwort1");
+    var a1 = document.getElementById("antwort1");
     var lsg = "689";
     if (a1.equals(lsg)){
         frage2.style.color = 'black';
