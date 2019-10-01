78522b3687b3a196a6f09533339024d995a2bf70
Garry <troet@live.de>
2019-10-01 18:18:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ var f2 = document.getElementById('f2');
 var f3 = document.getElementById('f3');
 
 function func1(){
-    var a = document.getElementById("a1");
-    if (a.value == "689"){
+    var a1 = document.getElementById("a1");
+    if (a1.value == "689"){
         f2.style.color = 'black';
     } else {
-        a.value = "";
+        a1.value = "";
     }
 function func1(){
-    var a = document.getElementById("a2");
-    if (a.value == "feiner Sand"){
+    var a2 = document.getElementById("a2");
+    if (a2.value == "feiner Sand"){
         f3.style.color = 'black';
-    } else if (a.value == "feiner sand") {
+    } else if (a2.value == "feiner sand") {
         f3.style.color = 'black';
     } else {
-        a.value = "";
+        a2.value = "";
     }   
 }
