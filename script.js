8b1b38e23d8a11cec4c4752579473e184d026f80
Garry <troet@live.de>
2019-10-01 18:17:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ var f2 = document.getElementById('f2');
 var f3 = document.getElementById('f3');
 
 function func1(){
-    var a1 = document.getElementById("a1");
-    if (a1.value == "689"){
+    var a = document.getElementById("a1");
+    if (a.value == "689"){
         f2.style.color = 'black';
     } else {
-        a1.value = "";
+        a.value = "";
     }
+function func1(){
+    var a = document.getElementById("a2");
+    if (a.value == "feiner Sand"){
+        f3.style.color = 'black';
+    } else if (a.value == "feiner sand") {
+        f3.style.color = 'black';
+    } else {
+        a.value = "";
+    }   
 }
