4cdcd0914a4006491eff94579c5afd46cd11c54e
Garry <troet@live.de>
2019-10-01 16:13:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var frage2 = document.getElementById('frage2');
 
 function func1(){
     var antwort = document.getElementById("antwort1");
-    //if (antwort == new String("689")){
-    frage2.style.color = 'black';
-    //}
+    if (antwort == new String("689")){
+        frage2.style.color = 'black';
+    }
 }
