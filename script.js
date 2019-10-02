a05bbf4c3427fc85b4535462e389263fd5bfc4b0
Garry <troet@live.de>
2019-10-02 21:55:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var f2 = document.getElementById('f2');
 var f3 = document.getElementById('f3');
+var f4 = document.getElementById('f4');
+var f5 = document.getElementById('f5');
+var f6 = document.getElementById('f6');
+var f7 = document.getElementById('f7');
+var f8 = document.getElementById('f8');
+var f9 = document.getElementById('f9');
+var f10 = document.getElementById('f10');
+var f11 = document.getElementById('f11');
 
 function func1(){
     var a1 = document.getElementById("a1");
@@ -13,12 +21,92 @@ function func1(){
 }
 function func2(){
     var a2 = document.getElementById("a2");
-    if (a2.value == "689"){
+    if (a2.value == "feiner Sand"){
         f3.style.color = 'black';
-    } else if (a2.value == "000"){
+    } else if (a2.value == "feiner sand"){
         f3.style.color = 'black';
     } else {
         a2.value = "";
+    }
+}
+function func3(){
+    var a3 = document.getElementById("a3");
+    if (a3.value == "feiner Sand"){
+        f4.style.color = 'black';
+    } else if (a3.value == "feiner sand"){
+        f4.style.color = 'black';
+    } else {
+        a3.value = "";
+    }
+}
+function func4(){
+    var a4 = document.getElementById("a4");
+    if (a4.value == "feiner Sand"){
+        f5.style.color = 'black';
+    } else if (a4.value == "feiner sand"){
+        f5.style.color = 'black';
+    } else {
+        a4.value = "";
+    }
+}
+function func5(){
+    var a5 = document.getElementById("a5");
+    if (a5.value == "feiner Sand"){
+        f6.style.color = 'black';
+    } else if (a5.value == "feiner sand"){
+        f6.style.color = 'black';
+    } else {
+        a5.value = "";
+    }
+}
+function func6(){
+    var a6 = document.getElementById("a6");
+    if (a6.value == "feiner Sand"){
+        f7.style.color = 'black';
+    } else if (a6.value == "feiner sand"){
+        f7.style.color = 'black';
+    } else {
+        a6.value = "";
+    }
+}
+function func7(){
+    var a7 = document.getElementById("a7");
+    if (a7.value == "feiner Sand"){
+        f8.style.color = 'black';
+    } else if (a7.value == "feiner sand"){
+        f8.style.color = 'black';
+    } else {
+        a7.value = "";
+    }
+}
+function func8(){
+    var a8 = document.getElementById("a8");
+    if (a8.value == "feiner Sand"){
+        f9.style.color = 'black';
+    } else if (a8.value == "feiner sand"){
+        f9.style.color = 'black';
+    } else {
+        a8.value = "";
+    }
+}
+function func9(){
+    var a9 = document.getElementById("a9");
+    if (a9.value == "feiner Sand"){
+        f10.style.color = 'black';
+    } else if (a9.value == "feiner sand"){
+        f10.style.color = 'black';
+    } else {
+        a9.value = "";
+    }
+}
+function func10(){
+    var a10 = document.getElementById("a10");
+    if (a10.value == "feiner Sand"){
+        f11.style.color = 'black';
+    } else if (a10.value == "feiner sand"){
+        f11.style.color = 'black';
+    } else {
+        a10.value = "";
     }
 }
 
